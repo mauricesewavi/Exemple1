2547fb24fd34e0f544137d4a41b342d33df47d36
Kokou Maurice SEWAVI <maurice.sewavi@gmail.com>
2021-10-26 16:59:22 +0000

    Comments and new output

--- src/main/java/ma/maurice/Test.java
+++ src/main/java/ma/maurice/Test.java
@@ -1,6 +1,10 @@
+/*Commentaires*/
 package ma.maurice;
 public class Test{
     public static void main(String[]args){
+        //Affichage d'une chaine de caracteres
         System.out.println("Seigneur me voici devant toi !");
+        System.out.println("Tout simplement pour te servir !");
+
     }
 }
