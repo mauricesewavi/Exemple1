dd98d4c3d53b3be143862140a7b9cf391f7709d0
Kokou Maurice SEWAVI <maurice.sewavi@gmail.com>
2021-10-27 12:47:13 +0000

    Patch code by kokou Maurice

--- src/main/java/ma/maurice/Test.java
+++ src/main/java/ma/maurice/Test.java
@@ -1,4 +1,7 @@
-/*Commentaires*/
+/**
+ * Commentaires
+ * Un autre commentaire
+ * */
 package ma.maurice;
 public class Test{
     public static void main(String[]args){
@@ -6,5 +9,8 @@ public class Test{
         System.out.println("Seigneur me voici devant toi !");
         System.out.println("Tout simplement pour te servir !");
 
+        int a=5, b=12;
+        int c = a+b;
+        System.out.println(c);
     }
 }
